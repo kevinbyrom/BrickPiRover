24495e68514511e62f5ddb909ca9f55cc2061d4f
Kevin Byrom <kevinbyrom@gmail.com>
2020-04-14 20:27:59 -0700

    Working single stick steering

--- steering/single_stick.py
+++ steering/single_stick.py
@@ -3,12 +3,27 @@ from controllers import ControllerBase
 
 deadzone = 0.05
 
+'''
+        0% <-> 100%
+         (1,1)
+           |
+           |
+ (-1,1) --- --- (1,-1)
+           |
+           |
+        (-1,-1)
+'''
 
 class SingleStickSteering():
     def apply(self, rover, controller):
         if abs(controller.left_stick_x) > deadzone or abs(controller.left_stick_y) > deadzone:
-            left_power = controller.left_stick_y - controller.left_stick_x
-            right_power = controller.left_stick_y + controller.left_stick_x
-            rover.thrust(left_power, right_power)
+            
+            thrust_pct = abs(controller.left_stick_y)
+            thrust_power = controller.left_stick_y * thrust_pct
+
+            left_turn_power = controller.left_stick_x * (1 - thrust_pct)
+            right_turn_power = -left_turn_power * (1 - thrust_pct)
+
+            rover.thrust(thrust_power + left_turn_power, thrust_power + right_turn_power) 
         else:
             rover.stop()
