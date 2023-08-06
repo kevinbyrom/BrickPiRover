564f7b0c73aa8923057f95db82c87c452cc8a4dd
kevinbyrom <kevinbyrom@gmail.com>
2023-08-06 05:30:41 +0000

    Fixed port

--- motor.py
+++ motor.py
@@ -1,10 +1,10 @@
 class Motor():
     def __init__(self, bp,  port):
         self.bp = bp
-        self.motor_port = port
+        self.port = port
 
     def reset(self):
         self.bp.offset_motor_encoder(self.port, self.bp.get_motor_encoder(self.port))
 
     def set_power(self, power):
-        self.bp.set_motor_power(self.port, self.power)
+        self.bp.set_motor_power(self.port, power)
