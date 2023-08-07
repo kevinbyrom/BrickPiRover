16731e792bd09fa5825e6c504322292ed7fdeb87
kevinbyrom <kevinbyrom@gmail.com>
2023-08-07 18:33:37 +0000

    Cleanup up component handling

--- rover.py
+++ rover.py
@@ -39,16 +39,18 @@ class Rover:
 
 
     def add_component(self, component):
-        component_type = type(component).__name__
-        self.components[component_type] = component
+        key = type(component).__name__
+        self.components[key] = component
 
 
     def get_component(self, component_type):
-        return self.components[component_type]
+        key = component_type.__name__
+        return self.components[key]
 
 
     def has_component(self, component_type):
-        return self.components[component_type] is not None
+        key = component_type.__name__
+        return self.components[key] is not None
     
 
     def get_all_components(self):
